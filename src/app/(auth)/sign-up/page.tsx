9f4328beab2916321858cccea126cf76a8f2b666
Joe Christie <joe.alec.christie@gmail.com>
2025-07-06 21:43:42 +0100

    chore: initial commit of NextJS form + validation (front end only)

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div className="bg-blue-500 w-full">Sign up</div>
+  )
+}
+
+export default page
---
+++ src/components/form/SignInForm.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";            // ← add this at the top
+
+
+import { useForm } from "react-hook-form"; // ← only this from RHF
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "../ui/form"; // ← shadcn wrapper
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+const formSchema = z.object({
+  email: z.string().min(1, 'Email is required').email('Invalid email'),
+  password: z
+    .string()
+    .min(1, 'Password is required')
+    .min(8, 'Password must have more than 8 characters'),
+});
+
+export default function SignInForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-3">
+        <div className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input className="bg-white" placeholder="mail@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-white"
+                    type="password"
+                    placeholder="Enter your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button className="w-full mt-6" type="submit">
+            Sign in
+        </Button>
+      </form>
+      <div className='mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400'>
+        or
+      </div>
+      <p className="text-center text-sm text-gray-600 mt-2">
+        If you don&apos;t have an account, please&nbsp;
+        <Link className='text-blue-500 hover:underline' href='/sign-up'>Sign up</Link>
+      </p>
+    </Form>
+  );
+}
